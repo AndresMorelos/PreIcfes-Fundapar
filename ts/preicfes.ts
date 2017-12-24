1f9a49a77462bf96f47848084e86b2e8be6edaf6
Andrés Morelos <andresmorelos123@gmail.com>
2017-12-24 13:12:59 -0500

    Using NPM
    Importing all JS with NPM and SystemJS.

--- ts/preicfes.ts
+++ ts/preicfes.ts
@@ -4,3 +4,13 @@
 * @Date: 23/12/2017
 * @Email: andresmorelos123@gmail.com
 */
+
+import "jquery";
+import "bootstrap";
+import "materialize-css";
+import "popper.js";
+
+
+$(document).ready(function(){
+  console.log("Pagina cargada");
+})
